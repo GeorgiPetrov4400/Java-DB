ad98646fdfea967dd2ac89ae05219d0884dd659d
Georgi Petrov <gap4400@gmail.com>
2022-09-20 10:40:03 +0300

    Added Task 11

---
+++ MySQL/Data Definition And Data Types Task11 - Exercise.sql
@@ -0,0 +1,44 @@
+USE `Movies`;
+
+CREATE TABLE `directors` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`director_name` VARCHAR(50) NOT NULL,
+`notes` TEXT
+);
+
+INSERT INTO `directors` (`director_name`, `notes`)
+VALUES ('Georgi', 'Note1'), ('Simona', 'Note2'), ('Siyana', 'Note3'), ('Martin', 'Note4'), ('Lukas', 'Note5');
+
+
+CREATE TABLE `genres` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`genre_name` VARCHAR(20) NOT NULL,
+`notes` TEXT
+);
+
+INSERT INTO `genres` (`genre_name`, `notes`)
+VALUES ('Georgi', 'Note1'), ('Simona', 'Note2'), ('Siyana', 'Note3'), ('Martin', 'Note4'), ('Lukas', 'Note5');
+
+CREATE TABLE `categories` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`category_name` VARCHAR(20) NOT NULL,
+`notes` TEXT
+);
+
+INSERT INTO `categories` (`category_name`, `notes`)
+VALUES ('Georgi', 'Note1'), ('Simona', 'Note2'), ('Siyana', 'Note3'), ('Martin', 'Note4'), ('Lukas', 'Note5');
+
+CREATE TABLE `movies` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`title` VARCHAR(40) NOT NULL,
+`director_id` INT,
+`copyright_year` INT,
+`lenght` INT,
+`genre_id` INT,
+`category_id` INT,
+`rating` DOUBLE,
+`notes` TEXT
+);
+
+INSERT INTO `movies` (`title`)
+VALUES ('Movie1'), ('Movie2'), ('Movie3'), ('Movie4'), ('Movie5');
