61e0dcc5b1e6c689bc1a0cf7688463bf3a0ce9bc
Georgi Petrov <gap4400@gmail.com>
2022-09-20 16:16:57 +0300

    Refactor Task 11

--- MySQL/Data Definition And Data Types Task11 - Exercise.sql
+++ MySQL/Data Definition And Data Types Task11 - Exercise.sql
@@ -1,9 +1,9 @@
 USE `Movies`;
 
 CREATE TABLE `directors` (
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`director_name` VARCHAR(50) NOT NULL,
-`notes` TEXT
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `director_name` VARCHAR(50) NOT NULL,
+    `notes` TEXT
 );
 
 INSERT INTO `directors` (`director_name`, `notes`) 
@@ -11,18 +11,18 @@ VALUES ('Name1', 'Note'), ('Name2', 'Note'), ('Name3', 'Note'), ('Name4', 'Note'
 
 
 CREATE TABLE `genres` (
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`genre_name` VARCHAR(20) NOT NULL,
-`notes` TEXT
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `genre_name` VARCHAR(20) NOT NULL,
+    `notes` TEXT
 );
 
 INSERT INTO `genres` (`genre_name`, `notes`)
 VALUES ('Name1', 'Note'), ('Name2', 'Note'), ('Name3', 'Note'), ('Name4', 'Note'), ('Name5', 'Note');
 
 CREATE TABLE `categories` (
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`category_name` VARCHAR(20) NOT NULL,
-`notes` TEXT
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `category_name` VARCHAR(20) NOT NULL,
+    `notes` TEXT
 );
 
 INSERT INTO `categories` (`category_name`, `notes`)
