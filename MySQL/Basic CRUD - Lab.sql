f497e90c8451d53d254549c59708d84feb965cba
Georgi Petrov <gap4400@gmail.com>
2022-09-21 11:10:45 +0300

    Added Basic CRUD - Lab All tasks

--- MySQL/Basic CRUD - Lab.sql
+++ MySQL/Basic CRUD - Lab.sql
@@ -51,3 +51,33 @@ CREATE TABLE clients (
 INSERT INTO clients(`first_name`,`last_name`,`room_id`) 
 VALUES('Pesho','Petrov', 1),('Gosho','Georgiev', 2),
 ('Mariya','Marieva', 2), ('Katya','Katerinova', 1), ('Nikolay','Nikolaev', 2);
+
+
+
+SELECT `id`, `first_name`, `last_name`, `job_title` 
+FROM `employees` 
+ORDER BY `id`;
+
+SELECT `id`, CONCAT(`first_name`, ' ', `last_name`) AS `full_name`, `job_title`, `salary` 
+FROM `employees` 
+WHERE `salary` > 1000
+ORDER BY `id`;
+
+SELECT `id`, `first_name`, `last_name`, `job_title`, `department_id`, `salary` 
+FROM `employees` 
+ORDER BY `salary` DESC
+LIMIT 1;
+
+SELECT `id`, `first_name`, `last_name`, `job_title`, `department_id`, `salary` 
+FROM `employees` 
+WHERE `department_id` = 4 AND `salary` >= 1000
+ORDER BY `id`;
+
+UPDATE `employees` 
+SET `salary` = `salary` + 100
+WHERE `job_title` = 'Manager';
+SELECT `salary` FROM `employees`;
+
+DELETE FROM `employees` 
+WHERE `department_id` = 1 OR `department_id` = 2;
+SELECT * FROM `employees`;
