ff1eeab96811582d029adb4de5ded644c958e1fb
Georgi Petrov <gap4400@gmail.com>
2022-09-25 11:22:02 +0300

    Added additional query solution

--- MySQL/Built-in Functions - Exercise.sql
+++ MySQL/Built-in Functions - Exercise.sql
@@ -32,3 +32,18 @@ SELECT * FROM `towns`
 WHERE `name` NOT LIKE 'r%' AND `name` NOT LIKE 'b%' AND `name` NOT LIKE 'd%'
 ORDER BY `name`;
 
+CREATE VIEW `v_employees_hired_after_2000` 
+AS SELECT `first_name`, `last_name` 
+FROM `employees` 
+WHERE YEAR(`hire_date`) > 2000;
+SELECT * FROM `v_employees_hired_after_2000`;
+
+SELECT `first_name`, `last_name` 
+FROM `employees` 
+WHERE CHAR_LENGTH(`last_name`) = 5;
+
+SELECT `country_name`, `iso_code` 
+FROM `countries`
+WHERE `country_name` LIKE '%a%a%a%' 
+ORDER BY `iso_code`;
+
