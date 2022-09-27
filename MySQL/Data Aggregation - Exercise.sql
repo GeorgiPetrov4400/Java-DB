3933eaa200488e4f7444480e466c772854de39b8
Georgi Petrov <gap4400@gmail.com>
2022-09-27 15:13:25 +0300

    Query from 1 to 10

--- MySQL/Data Aggregation - Exercise.sql
+++ MySQL/Data Aggregation - Exercise.sql
@@ -185,3 +185,60 @@ INSERT INTO `wizzard_deposits` (`id`, `first_name`, `last_name`, `notes`, `age`,
 	(161, 'Yaxley', '', 'Death Eater, Head of Magical Law Enforcement under Voldemort\'s regime.', 38, 'Jimmy Kiddell', 16, 'Troll Chest', '1988-01-03', 26638.92, 7.11, 74.00, '1988-02-21', b'1'),
 	(162, 'Blaise', 'Zabini', 'Slytherin student in Harry\'s year, friends with Draco Malfoy.', 16, 'Mykew Gregorovitch', 11, 'Human Pride', '1990-03-28', 3049.06, 6.27, 61.00, '1990-07-04', b'0');
 
+SELECT COUNT(*) AS 'count' FROM `wizzard_deposits`;
+
+SELECT `magic_wand_size` AS 'longest_magic_wand' FROM `wizzard_deposits`
+GROUP BY `magic_wand_size`
+ORDER BY `magic_wand_size` DESC
+LIMIT 1;
+
+SELECT `deposit_group`, MAX(`magic_wand_size`) AS 'longest_magic_wand' FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand`, `deposit_group`;
+
+SELECT `deposit_group` FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY AVG(`magic_wand_size`)
+LIMIT 1;
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS "total_sum" FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum`;
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS "total_sum" FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = "Ollivander family"
+GROUP BY `deposit_group`
+ORDER BY `deposit_group`;
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS "total_sum" FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = "Ollivander family"
+GROUP BY `deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
+
+SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS 'min_deposit_charge' 
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`, `magic_wand_creator`
+ORDER BY `magic_wand_creator`, `deposit_group`;
+
+SELECT 
+CASE 
+WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+WHEN `age` >= 61 THEN '[61+]' 
+END
+AS 'age_group', 
+COUNT(`age`) AS 'wizzards_count'
+FROM `wizzard_deposits`
+GROUP BY `age_group`
+ORDER BY `wizzards_count`;
+
+SELECT LEFT(`first_name`, 1) AS 'first_letter' FROM `wizzard_deposits`
+WHERE `deposit_group` = "Troll Chest"
+GROUP BY `first_letter`
+ORDER BY `first_letter`;
+
