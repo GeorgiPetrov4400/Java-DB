5e321c8841e5f4967c0d0ce4ef51bd0e42bf0dd2
Georgi Petrov <gap4400@gmail.com>
2022-09-27 15:40:14 +0300

    Query from 1 to 11

--- MySQL/Data Aggregation - Exercise.sql
+++ MySQL/Data Aggregation - Exercise.sql
@@ -242,3 +242,8 @@ WHERE `deposit_group` = "Troll Chest"
 GROUP BY `first_letter`
 ORDER BY `first_letter`;
 
+SELECT `deposit_group`, `is_deposit_expired`, AVG(`deposit_interest`) AS 'average_interest'
+FROM `wizzard_deposits`
+WHERE DATE(`deposit_start_date`) > '1985-01-01'
+GROUP BY `deposit_group`, `is_deposit_expired`
+ORDER BY `deposit_group` DESC, `is_deposit_expired`;
