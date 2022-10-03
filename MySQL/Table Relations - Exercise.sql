cbbce9b6b54a6bad1de38da1c524580edb4d6a1a
Georgi Petrov <gap4400@gmail.com>
2022-10-03 12:59:30 +0300

    One to many relationship

--- MySQL/Table Relations - Exercise.sql
+++ MySQL/Table Relations - Exercise.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE `exercise`;
 
+#1 One to one relationship
 CREATE TABLE `passports` (
 `passport_id` INT PRIMARY KEY AUTO_INCREMENT,
 `passport_number` VARCHAR(50) UNIQUE
@@ -20,3 +21,31 @@ REFERENCES `passports`(`passport_id`)
 
 INSERT INTO `people`(`person_id`, `first_name`, `salary`, `passport_id`) 
 VALUES (1, 'Roberto', 43300.00, 102), (2, 'Tom', 56100.00, 103), (3, 'Yana', 60200.00, 101);
+
+
+#2 One to many relationship
+CREATE TABLE `manufacturers` (
+`manufacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) UNIQUE,
+`established_on` DATE
+);
+
+INSERT INTO `manufacturers`(`manufacturer_id`, `name`, `established_on`) 
+VALUES (1, 'BMW', '1916/03/01'), (2, 'Tesla', '2003/01/01'), (3, 'Lada', '1966/05/01');
+
+CREATE TABLE `models` (
+`model_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) UNIQUE,
+`manufacturer_id` INT,
+CONSTRAINT `fk_models_manufacturers`
+FOREIGN KEY (`manufacturer_id`)
+REFERENCES `manufacturers`(`manufacturer_id`)
+);
+
+INSERT INTO `models`(`model_id`, `name`, `manufacturer_id`) 
+VALUES (101, 'X1', 1), 
+(102, 'i6', 1), 
+(103, 'Model S', 2), 
+(104, 'Model X', 2), 
+(105, 'Model 3', 2), 
+(106, 'Nova', 3);
