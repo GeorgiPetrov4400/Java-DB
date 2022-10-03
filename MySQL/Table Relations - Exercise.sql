1dccebf0f27b787ef0207a9c756ad4cd8a2260f1
Georgi Petrov <gap4400@gmail.com>
2022-10-03 14:13:22 +0300

    Many to many relationship

--- MySQL/Table Relations - Exercise.sql
+++ MySQL/Table Relations - Exercise.sql
@@ -49,3 +49,40 @@ VALUES (101, 'X1', 1),
 (104, 'Model X', 2), 
 (105, 'Model 3', 2), 
 (106, 'Nova', 3);
+
+
+#3 Many to many relationship
+CREATE TABLE `students` (
+`student_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50)
+);
+
+INSERT INTO `students`(`student_id`, `name`) VALUES (1, 'Mila'), (2, 'Toni'), (3, 'Ron'); 
+
+CREATE TABLE `exams` (
+`exam_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) UNIQUE
+);
+
+INSERT INTO `exams`(`exam_id`, `name`) VALUES (101, 'Spring MVC'), (102, 'Neo4j'), (103, 'Oracle 11g');
+
+CREATE TABLE `students_exams` (
+`student_id` INT NOT NULL,
+`exam_id` INT NOT NULL,
+CONSTRAINT `pk_students_exams`
+PRIMARY KEY (`student_id`, `exam_id`),
+CONSTRAINT `fk_this_student`
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`), 
+CONSTRAINT `fk_this_exam`
+FOREIGN KEY (`exam_id`)
+REFERENCES `exams`(`exam_id`)
+);
+
+INSERT INTO `students_exams`(`student_id`, `exam_id`) 
+VALUES (1, 101), 
+(1, 102), 
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
