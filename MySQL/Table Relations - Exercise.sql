6c5847faae751727de0f8038a521a0b2e7657ea5
Georgi Petrov <gap4400@gmail.com>
2022-10-03 15:17:52 +0300

    Self-Referencing

--- MySQL/Table Relations - Exercise.sql
+++ MySQL/Table Relations - Exercise.sql
@@ -86,3 +86,24 @@ VALUES (1, 101),
 (3, 103),
 (2, 102),
 (2, 103);
+
+#4 Self-referencing
+CREATE TABLE `teachers` (
+`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`manager_id` INT
+);
+
+INSERT INTO `teachers`(`teacher_id`,`name`, `manager_id`) 
+VALUES (101, 'John', NULL), 
+(102, 'Maya', 106), 
+(103, 'Silvia', 106), 
+(104, 'Ted', 105), 
+(105, 'Mark', 101), 
+(106, 'Greta', 101);
+
+ALTER TABLE `teachers` 
+ADD CONSTRAINT `fk_teacher_manager`
+FOREIGN KEY (`manager_id`) 
+REFERENCES `teachers`(`teacher_id`);
+
