d7f7853ece804a6931cbea666b8b314cef960143
Georgi Petrov <gap4400@gmail.com>
2022-10-04 09:51:48 +0300

    Peaks in Rila

--- MySQL/Table Relations - Exercise.sql
+++ MySQL/Table Relations - Exercise.sql
@@ -206,3 +206,11 @@ CONSTRAINT `fk_agenda_subjects`
 FOREIGN KEY (`subject_id`)
 REFERENCES `subjects`(`subject_id`)
 );
+
+#9 Peaks in Rila
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` AS 'peak_elevation' 
+FROM `mountains` AS m
+JOIN `peaks` AS p
+ON m.`id` = p.`mountain_id`
+WHERE m.`mountain_range` = 'Rila'
+ORDER BY `peak_elevation` DESC;
