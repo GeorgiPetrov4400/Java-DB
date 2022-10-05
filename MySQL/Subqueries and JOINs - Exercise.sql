f305446e3a4d26a098f8aeb00c5107773b5eedb8
Georgi Petrov <gap4400@gmail.com>
2022-10-05 13:13:11 +0300

    Sales Employee

--- MySQL/Subqueries and JOINs - Exercise.sql
+++ MySQL/Subqueries and JOINs - Exercise.sql
@@ -15,3 +15,9 @@ ORDER BY `first_name`, `last_name`
 LIMIT 5;
 
 #3 Sales Employee
+SELECT e.`employee_id`, e.`first_name`, e.`last_name`, d.`name` AS 'department_name'
+FROM `employees` AS e
+JOIN `departments` AS d
+ON e.`department_id` = d.`department_id`
+WHERE d.`name` = 'Sales'
+ORDER BY `employee_id` DESC;
