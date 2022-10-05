f9e4853779f5ec58f1d5c16cf8bb0829e31b9cb0
Georgi Petrov <gap4400@gmail.com>
2022-10-05 15:52:59 +0300

    Employee Manager

--- MySQL/Subqueries and JOINs - Exercise.sql
+++ MySQL/Subqueries and JOINs - Exercise.sql
@@ -65,3 +65,11 @@ JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
 WHERE e.`employee_id` = 24
 ORDER BY p.`name`; 
 
+#9 Employee Manager
+SELECT e1.`employee_id`, e1.`first_name`, e1.`manager_id`, e2.`first_name` 
+FROM `employees` AS e1
+JOIN `employees` AS e2
+ON e1.`manager_id` = e2.`employee_id`
+WHERE e1.`manager_id` IN(3, 7)
+ORDER BY e1.`first_name`;
+
