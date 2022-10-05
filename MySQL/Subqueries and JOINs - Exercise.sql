f9bab6371ebf39d5253d9974f68417a13524a5a0
Georgi Petrov <gap4400@gmail.com>
2022-10-05 14:02:42 +0300

    Employees with Project

--- MySQL/Subqueries and JOINs - Exercise.sql
+++ MySQL/Subqueries and JOINs - Exercise.sql
@@ -48,3 +48,12 @@ ON e.`department_id` = d.`department_id`
 WHERE `hire_date` > 1999-01-01 AND (d.`name` = 'Sales' OR d.`name` = 'Finance')
 ORDER BY `hire_date`;
 
+#7 Employees with Project
+SELECT e.`employee_id`, e.`first_name`, p.`name` AS 'project_name'
+FROM `employees` AS e
+JOIN `employees_projects` AS ep ON e.`employee_id` = ep.`employee_id`
+JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
+WHERE DATE(p.`start_date`) > '2002-08-13' AND p.`end_date` IS NULL
+ORDER BY e.`first_name`, p.`name`
+LIMIT 5;
+
