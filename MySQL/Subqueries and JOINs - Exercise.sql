e9b3f61bf10e6ebc92f13f8f880efc6f56dc7715
Georgi Petrov <gap4400@gmail.com>
2022-10-05 16:03:50 +0300

    Employee Summary

--- MySQL/Subqueries and JOINs - Exercise.sql
+++ MySQL/Subqueries and JOINs - Exercise.sql
@@ -73,3 +73,14 @@ ON e1.`manager_id` = e2.`employee_id`
 WHERE e1.`manager_id` IN(3, 7)
 ORDER BY e1.`first_name`;
 
+#10 Employee Summary
+SELECT e1.`employee_id`, 
+		CONCAT(e1.`first_name`, ' ', e1.`last_name`) AS 'employee_name', 
+		CONCAT(e2.`first_name`, ' ', e2.`last_name`) AS 'manager_name',
+		d.`name` AS 'department_name'
+FROM `employees` AS e1
+JOIN `employees` AS e2 ON e1.`manager_id` = e2.`employee_id`
+JOIN `departments` AS d ON d.`department_id` = e1.`department_id`
+ORDER BY e1.`employee_id`
+LIMIT 5;
+
