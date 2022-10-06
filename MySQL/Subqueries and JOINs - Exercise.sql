8d9735f150834267105653c4d9b136623f7cffc4
Georgi Petrov <gap4400@gmail.com>
2022-10-06 09:27:44 +0300

    Highest Peak and Longest River by Country

--- MySQL/Subqueries and JOINs - Exercise.sql
+++ MySQL/Subqueries and JOINs - Exercise.sql
@@ -132,3 +132,13 @@ LEFT JOIN `mountains_countries` AS mc ON mc.`country_code` = c.`country_code`
 LEFT JOIN `mountains` AS m ON m.`id` = mc.`mountain_id`
 WHERE m.`id` IS NULL;
 
+#17 Highest Peak and Longest River by Country
+SELECT c.`country_name`, MAX(p.`elevation`) AS 'highest_peak_elevation', MAX(r.`length`) AS 'longest_river_legth' 
+FROM `countries` AS c
+JOIN `mountains_countries` AS mc ON c.`country_code` = mc.`country_code`
+JOIN `peaks` AS p ON mc.`mountain_id` = p.`mountain_id`
+JOIN `countries_rivers` AS cr ON c.`country_code` = cr.`country_code`
+JOIN `rivers` AS r ON cr.`river_id` = r.`id`
+GROUP BY c.`country_name`
+ORDER BY `highest_peak_elevation` DESC, `longest_river_legth` DESC, c.`country_name`
+LIMIT 5;
