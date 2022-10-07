02aba01223814d600b7fea56c02764d5e8897f8a
Georgi Petrov <gap4400@gmail.com>
2022-10-07 15:08:49 +0300

    Salary Level Function

--- MySQL/Database Programmability and Transactions - Exercise.sql
+++ MySQL/Database Programmability and Transactions - Exercise.sql
@@ -34,3 +34,19 @@ BEGIN
     ORDER BY e.`first_name`, e.`last_name`, e.`employee_id`;
 END%%
 
+#5 Salary Level Function
+CREATE FUNCTION ufn_get_salary_level (employee_salary DECIMAL(19, 4))
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+	DECLARE salary_level VARCHAR(10);
+    
+    IF employee_salary < 30000 THEN SET salary_level := 'Low';
+    ELSEIF employee_salary <= 50000 THEN SET salary_level = 'Average';
+    ELSE SET salary_level = 'High';
+    END IF;
+
+	RETURN salary_level;
+END%%
+
+
