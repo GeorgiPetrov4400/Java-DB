6bef66a40598a5fbd69a8125edd34948a5b63531
Georgi Petrov <gap4400@gmail.com>
2022-10-07 15:17:39 +0300

    Employees by Salary Level

--- MySQL/Database Programmability and Transactions - Exercise.sql
+++ MySQL/Database Programmability and Transactions - Exercise.sql
@@ -49,4 +49,11 @@ BEGIN
 	RETURN salary_level;
 END%%
 
+#6 Employees by Salary Level
+CREATE PROCEDURE usp_get_employees_by_salary_level (salary_level VARCHAR(10))
+BEGIN
+	SELECT `first_name`, `last_name` FROM `employees`
+    WHERE ufn_get_salary_level(`salary`) LIKE salary_level
+    ORDER BY `first_name` DESC, `last_name` DESC;
+END%%
 
