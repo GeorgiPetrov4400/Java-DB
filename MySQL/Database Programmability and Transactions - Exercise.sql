eb30a950101cfd2b4bd9fc44422f50ff046977ab
Georgi Petrov <gap4400@gmail.com>
2022-10-07 14:01:51 +0300

    Employees from Town

--- MySQL/Database Programmability and Transactions - Exercise.sql
+++ MySQL/Database Programmability and Transactions - Exercise.sql
@@ -24,3 +24,13 @@ BEGIN
     ORDER BY `name`;
 END%%
 
+#4 Employees from Town
+CREATE PROCEDURE usp_get_employees_from_town (enter_town VARCHAR(50))
+BEGIN
+	SELECT e.`first_name`, e.`last_name` FROM `employees` AS e
+    JOIN `addresses` AS a ON e.`address_id` = a.`address_id`
+    JOIN `towns` AS t ON a.`town_id` = t.`town_id`
+    WHERE t.`name` = enter_town
+    ORDER BY e.`first_name`, e.`last_name`, e.`employee_id`;
+END%%
+
