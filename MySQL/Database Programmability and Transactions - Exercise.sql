04d8d8f58bdd1a0d10c430d8d1f80c4e576b23bc
Georgi Petrov <gap4400@gmail.com>
2022-10-10 14:48:22 +0300

    Emails Trigger

--- MySQL/Database Programmability and Transactions - Exercise.sql
+++ MySQL/Database Programmability and Transactions - Exercise.sql
@@ -164,3 +164,20 @@ BEGIN
     VALUES (OLD.`id`, OLD.`balance`, NEW.`balance`);
 END%%
 
+#16 Emails Trigger 
+CREATE TABLE `notification_emails` (
+`id` INT PRIMARY KEY AUTO_INCREMENT, 
+`recipient` INT NOT NULL,
+`subject` TEXT,
+`body` TEXT
+);
+
+DELIMITER%%
+CREATE TRIGGER tr_email_logs
+AFTER INSERT ON `logs`
+FOR EACH ROW
+BEGIN
+	INSERT INTO `notification_emails`(`recipient`, `subject`, `body`)
+    VALUES (NEW.`account_id`, concat_ws(' ', 'Balance change for account:', NEW.`account_id`), 
+    concat_ws(' ', 'On', NOW(), 'your balance was changed from', NEW.`old_sum`, 'to', NEW.`new_sum`, '.'));
+END%%
