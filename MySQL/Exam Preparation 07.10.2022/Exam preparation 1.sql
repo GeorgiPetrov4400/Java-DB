9b5647b9e80fc45218d24defa361e00299e3c3ea
Georgi Petrov <gap4400@gmail.com>
2022-10-11 16:34:30 +0300

    The fastest player by towns

--- MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
+++ MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
@@ -647,3 +647,13 @@ SELECT t.`name` AS 'team_name', t.`established`, t.`fan_base`, COUNT(p.`id`) AS 
 LEFT JOIN `players` AS p ON p.`team_id` = t.`id`
 GROUP BY t.`id`
 ORDER BY `players_count` DESC, t.`fan_base` DESC;
+
+#8 The fastest player by towns
+SELECT MAX(sd.`speed`) AS 'max_speed', tw.`name` AS 'town_name' FROM `skills_data` AS sd
+RIGHT JOIN `players` AS p ON sd.`id` = p.`skills_data_id`
+RIGHT JOIN `teams` AS t ON p.`team_id` = t.`id`
+RIGHT JOIN `stadiums` AS s ON t.`stadium_id` = s.`id`
+RIGHT JOIN `towns` AS tw ON s.`town_id` = tw.`id`
+WHERE t.`name` <> 'Devify'
+GROUP BY tw.`id`
+ORDER BY `max_speed` DESC, `town_name`;
