3614a2f20dcc94b276f393ec34df074298581990
Georgi Petrov <gap4400@gmail.com>
2022-10-11 18:21:00 +0300

    Find good playmaker by teams

--- MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
+++ MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
@@ -682,3 +682,14 @@ BEGIN
     
 END%%
 
+#11 Find good playmaker by teams
+CREATE PROCEDURE udp_find_playmaker (min_dribble_points INT, team_name VARCHAR(45)) 
+BEGIN 
+	SELECT CONCAT(p.`first_name`, ' ', p.`last_name`) AS 'full_name', p.`age`, p.`salary`, sd.`dribbling`, sd.`speed`, t.`name` 
+    FROM `players` AS p
+    JOIN `skills_data` AS sd ON p.`skills_data_id` = sd.`id`
+    JOIN `teams` AS t ON p.`team_id` = t.`id`
+    WHERE t.`name` = team_name AND sd.`dribbling` > min_dribble_points AND sd.`speed` > (SELECT AVG(`speed`) FROM `skills_data`)
+    ORDER BY sd.`speed` DESC
+    LIMIT 1;
+END%%
