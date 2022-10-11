efcabe6d86487b02ca703df602472c6b5717feab
Georgi Petrov <gap4400@gmail.com>
2022-10-11 16:59:18 +0300

    Total salaries and players by country

--- MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
+++ MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
@@ -657,3 +657,12 @@ RIGHT JOIN `towns` AS tw ON s.`town_id` = tw.`id`
 WHERE t.`name` <> 'Devify'
 GROUP BY tw.`id`
 ORDER BY `max_speed` DESC, `town_name`;
+
+#9 Total salaries and players by country
+SELECT c.`name`, COUNT(p.`id`) AS 'total_count_of_players', SUM(p.`salary`) AS 'total_sum_of_salaries' FROM `countries` AS c
+LEFT JOIN `towns` AS t ON c.`id` = t.`country_id`
+LEFT JOIN `stadiums` AS s ON t.`id` = s.`town_id`
+LEFT JOIN `teams` AS tm ON s.`id` = tm.`stadium_id`
+LEFT JOIN `players` AS p ON tm.`id` = p.`team_id`
+GROUP BY c.`id`
+ORDER BY `total_count_of_players` DESC, c.`name`;
