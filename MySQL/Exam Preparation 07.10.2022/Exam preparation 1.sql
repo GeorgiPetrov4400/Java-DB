9249a3f746e4702f0353ebb294cb2d8afb4c7f52
Georgi Petrov <gap4400@gmail.com>
2022-10-11 11:44:16 +0300

    Update

--- MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
+++ MySQL/Exam Preparation 07.10.2022/Exam preparation 1.sql
@@ -622,4 +622,9 @@ INSERT INTO `coaches` (`first_name`, `last_name`, `salary`, `coach_level`)
  FROM `players` AS p
  WHERE `age` >= 45);
  
- 
+#3 Update
+UPDATE `coaches` AS c 
+SET c.`coach_level` = c.`coach_level` + 1
+WHERE c.`id` IN (SELECT `coach_id` FROM `players_coaches`) AND c.`first_name` LIKE 'A%';
+
+
