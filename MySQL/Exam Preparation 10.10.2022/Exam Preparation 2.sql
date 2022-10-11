1d8eb4f4a8e67b6bad032f9483b9f04fc7a530d2
Georgi Petrov <gap4400@gmail.com>
2022-10-11 23:15:32 +0300

    Movie casting

--- MySQL/Exam Preparation 10.10.2022/Exam Preparation 2.sql
+++ MySQL/Exam Preparation 10.10.2022/Exam Preparation 2.sql
@@ -105,3 +105,11 @@ WHERE YEAR(ai.`release_date`) BETWEEN 1996 AND 1999
 ORDER BY ai.`runtime`, m.`id`
 LIMIT 20;
 
+#7 Movie casting
+SELECT CONCAT(a.`first_name`, ' ', a.`last_name`) AS 'full_name', CONCAT(REVERSE(a.`last_name`), LENGTH(a.`last_name`), '@cast.com') AS 'email',
+	2022 - YEAR(a.`birthdate`) AS 'age', a.`height` FROM `actors` AS a
+    LEFT JOIN `movies_actors` AS ma ON a.`id` = ma.`actor_id`
+    WHERE ma.`actor_id` IS NULL
+    GROUP BY a.`id`
+    ORDER BY a.`height`;
+    
