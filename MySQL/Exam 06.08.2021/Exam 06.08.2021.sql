8acfeb8f2c2dab99aed07d97a3dc873d89953fb7
Georgi Petrov <gap4400@gmail.com>
2022-10-12 21:27:10 +0300

    Games of 2022

--- MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
+++ MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
@@ -789,3 +789,17 @@ JOIN `games` AS g ON gc.`game_id` = g.`id`
 GROUP BY c.`name`
 HAVING `max_rating` >= 9.5
 ORDER BY `games_count` DESC, c.`name`;
+
+#8 Games of 2022
+SELECT  g.`name`, 
+		g.`release_date`, CONCAT(SUBSTRING(g.`description`, 1, 10), '...') AS 'summary', 
+		(CASE
+         WHEN MONTH(g.`release_date`) BETWEEN 1 AND 3 THEN 'Q1'
+         WHEN MONTH(g.`release_date`) BETWEEN 4 AND 6 THEN 'Q2'
+         WHEN MONTH(g.`release_date`) BETWEEN 7 AND 9 THEN 'Q3'
+         WHEN MONTH(g.`release_date`) BETWEEN 10 AND 12 THEN 'Q4'
+         END) AS 'quarter', 
+        t.`name` AS 'team_name' FROM `games` AS g
+JOIN `teams` AS t ON g.`team_id` = t.`id` 
+WHERE g.`name` LIKE '%2' AND YEAR(g.`release_date`) = 2022 AND MONTH(g.`release_date`) % 2 = 0
+ORDER BY `quarter`;
