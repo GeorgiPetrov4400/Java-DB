d93a570c19398c565ae3c1fbce5ad64df9edb563
Georgi Petrov <gap4400@gmail.com>
2022-10-12 22:17:17 +0300

    Find all basic information for a game

--- MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
+++ MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
@@ -814,3 +814,16 @@ LEFT JOIN `games_categories` AS gc ON g.`id` = gc.`game_id`
 LEFT JOIN `categories` AS c ON gc.`category_id` = c.`id`
 WHERE g.`release_date` IS NULL AND c.`id` IS NULL
 ORDER BY g.`name`;
+
+#10 Find all basic information for a game
+DELIMITER %%
+CREATE FUNCTION udf_game_info_by_name (game_name VARCHAR (20)) 
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+	RETURN (SELECT CONCAT_WS(' ','The', g.`name`, 'is developed by a', t.`name`, 'in an office with an address', a.`name`) FROM `games` AS g
+	JOIN `teams` AS t ON g.`team_id` = t.`id`
+	JOIN `offices` AS o ON t.`office_id` = o.`id`
+	JOIN `addresses` AS a ON o.`address_id` = a.`id`
+	WHERE g.`name` = game_name);
+END %%
