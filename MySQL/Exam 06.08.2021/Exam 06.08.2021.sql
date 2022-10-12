1a75bb8a9c2e1bc1e49355f6dc508073cfbc0ecf
Georgi Petrov <gap4400@gmail.com>
2022-10-12 20:48:07 +0300

    Categories Info

--- MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
+++ MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
@@ -781,3 +781,11 @@ JOIN `offices` AS o ON t.`office_id` = o.`id`
 JOIN `addresses` AS a ON o.`address_id` = a.`id`
 WHERE o.`website` IS NOT NULL
 ORDER BY `team_name`, `address_name`;
+
+#7 Categories Info
+SELECT c.`name`, COUNT(g.`id`) AS 'games_count', ROUND(AVG(g.`budget`), 2) AS 'avg_budget', MAX(g.`rating`) AS 'max_rating' FROM `categories` AS c
+JOIN `games_categories` AS gc ON c.`id` = gc.`category_id`
+JOIN `games` AS g ON gc.`game_id` = g.`id`
+GROUP BY c.`name`
+HAVING `max_rating` >= 9.5
+ORDER BY `games_count` DESC, c.`name`;
