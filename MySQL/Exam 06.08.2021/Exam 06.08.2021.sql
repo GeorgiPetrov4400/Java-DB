831af4446e29afaefad87d29dceec871800a8935
Georgi Petrov <gap4400@gmail.com>
2022-10-12 21:47:26 +0300

    Full info for games

--- MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
+++ MySQL/Exam 06.08.2021/Exam 06.08.2021.sql
@@ -803,3 +803,14 @@ SELECT  g.`name`,
 JOIN `teams` AS t ON g.`team_id` = t.`id` 
 WHERE g.`name` LIKE '%2' AND YEAR(g.`release_date`) = 2022 AND MONTH(g.`release_date`) % 2 = 0
 ORDER BY `quarter`;
+
+#9 Full info for games
+SELECT g.`name`, IF(g.`budget` <= 50000, 'Normal budget', 'Insufficient budget') AS 'budget_level', 
+t.`name` AS 'team_name', a.`name` AS 'address_name' FROM `games` AS g
+JOIN `teams` AS t ON g.`team_id` = t.`id`
+JOIN `offices` AS o ON t.`office_id` = o.`id`
+JOIN `addresses` AS a ON o.`address_id` = a.`id`
+LEFT JOIN `games_categories` AS gc ON g.`id` = gc.`game_id`
+LEFT JOIN `categories` AS c ON gc.`category_id` = c.`id`
+WHERE g.`release_date` IS NULL AND c.`id` IS NULL
+ORDER BY g.`name`;
