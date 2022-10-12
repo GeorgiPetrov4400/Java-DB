49a596d1e64b5ebda9f6b1ccd6179a3c599743d1
Georgi Petrov <gap4400@gmail.com>
2022-10-12 13:58:54 +0300

    Review

--- MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
+++ MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
@@ -98,3 +98,8 @@ ORDER BY `name` DESC;
 SELECT `id`, `brand_id`, `name`, `quantity_in_stock` FROM `products`
 WHERE `price` > 1000 AND `quantity_in_stock` < 30
 ORDER BY `quantity_in_stock`, `id`;
+
+#7 Review
+SELECT `id`, `content`, `rating`, `picture_url`, `published_at` FROM `reviews`
+WHERE `content` LIKE "My%" AND LENGTH(`content`) > 61
+ORDER BY `rating` DESC;
