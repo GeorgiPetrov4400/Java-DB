22c08e389471b0d06a10430e537fe21acf5ee22c
Georgi Petrov <gap4400@gmail.com>
2022-10-12 15:32:11 +0300

    Extract client cards count

--- MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
+++ MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
@@ -116,3 +116,18 @@ JOIN `products` AS p ON c.`id` = p.`category_id`
 GROUP BY p.`category_id`
 ORDER BY `items_count` DESC, `total_quantity`
 LIMIT 5;
+
+#10 Extract client cards count
+DELIMITER %%
+CREATE FUNCTION udf_customer_products_count(name VARCHAR(30)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE products_count INT;
+    SET products_count := (SELECT COUNT(p.`id`) AS 'total_products' FROM `customers` AS c
+						   JOIN `orders` AS o ON c.`id` = o.`customer_id`
+						   JOIN `orders_products` AS op ON o.`id` = op.`order_id`
+						   JOIN `products` AS p ON op.`product_id` = p.`id`
+						   WHERE c.`first_name` = name);
+    RETURN products_count;
+END %%
