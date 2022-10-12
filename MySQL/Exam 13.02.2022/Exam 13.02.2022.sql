849db34feee575d63b57318cc3575b3680a54ba7
Georgi Petrov <gap4400@gmail.com>
2022-10-12 14:33:03 +0300

    First customers

--- MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
+++ MySQL/Exam 13.02.2022/Exam 13.02.2022.sql
@@ -103,3 +103,9 @@ ORDER BY `quantity_in_stock`, `id`;
 SELECT `id`, `content`, `rating`, `picture_url`, `published_at` FROM `reviews`
 WHERE `content` LIKE "My%" AND LENGTH(`content`) > 61
 ORDER BY `rating` DESC;
+
+#8 First customers
+SELECT CONCAT(c.`first_name`, ' ', c.`last_name`) AS 'full_name', c.`address`, o.`order_datetime` AS 'order_date' FROM `customers` AS c
+JOIN `orders` AS o ON c.`id` = o.`customer_id`
+WHERE YEAR(o.`order_datetime`) <= 2018
+ORDER BY `full_name` DESC;
