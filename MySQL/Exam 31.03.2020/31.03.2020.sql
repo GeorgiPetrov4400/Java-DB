1c0dbf2c7ba5a6984b45f688333c775b7eb6e5a6
Georgi Petrov <gap4400@gmail.com>
2022-10-14 17:26:06 +0300

    All queries

--- MySQL/Exam 31.03.2020/31.03.2020.sql
+++ MySQL/Exam 31.03.2020/31.03.2020.sql
@@ -81,3 +81,54 @@ WHERE `country` LIKE 'B%' OR `country` LIKE 'T%' OR `country` LIKE 'P%';
 #4 Delete
 DELETE a FROM `addresses` AS a
 WHERE a.`id` % 3 = 0;
+
+#5 Users
+SELECT `username`, `gender`, `age` FROM `users`
+ORDER BY `age` DESC, `username`;
+
+#6 Extract 5 most commented photos
+SELECT p.`id`, p.`date` AS 'date_and_time', p.`description`, COUNT(c.`id`) AS 'commentsCount' FROM `photos` AS p
+JOIN `comments` AS c ON p.`id` = c.`photo_id`
+GROUP BY p.`id`
+ORDER BY `commentsCount` DESC, p.`id`
+LIMIT 5;
+
+#7 Lucky users
+SELECT CONCAT(u.`id`, ' ', u.`username`) AS 'id_username', u.`email` FROM `users` AS u
+JOIN `users_photos` AS up ON u.`id` = up.`user_id`
+WHERE up.`photo_id` = up.`user_id`
+ORDER BY u.`id`;
+
+#8 Count likes and comments
+SELECT p.`id` AS 'photo_id', 
+(SELECT COUNT(l.`id`) FROM `likes` AS l WHERE p.`id` = l.`photo_id`) AS 'likes_count', 
+(SELECT COUNT(c.`id`) FROM `comments` AS c WHERE p.`id` = c.`photo_id`) AS 'comments_count' 
+FROM `photos` AS p
+GROUP BY p.`id`
+ORDER BY `likes_count` DESC, `comments_count` DESC, p.`id`;
+
+#9 The photo on the tenth day of the month
+SELECT CONCAT(SUBSTRING(`description`, 1, 30), '...') AS 'summary', `date` FROM `photos`
+WHERE DAY(`date`) = 10
+ORDER BY `date` DESC;
+
+#10 Get user’s photos count
+DELIMITER %%
+CREATE FUNCTION udf_users_photos_count(username VARCHAR(30)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN (SELECT COUNT(p.`id`) FROM `users` AS u
+			JOIN `users_photos` AS up ON u.`id` = up.`user_id`
+			JOIN `photos` AS p ON up.`photo_id` = p.`id`
+			WHERE u.`username` = username);
+END %%
+
+#11 Increase user age
+CREATE PROCEDURE udp_modify_user (address VARCHAR(30), town VARCHAR(30)) 
+BEGIN
+	UPDATE `users` AS u
+	JOIN `addresses` AS a ON u.`id` = a.`user_id`
+	SET u.`age` = u.`age` + 10
+	WHERE a.`address` = address AND a.`town` = town;
+END %%
