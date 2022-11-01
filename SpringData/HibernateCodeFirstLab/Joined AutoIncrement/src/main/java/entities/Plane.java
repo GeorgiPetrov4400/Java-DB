5b67314c7c16da61e69158cec7010485c4346e87
Georgi Petrov <gap4400@gmail.com>
2022-11-01 13:40:23 +0200

    Joined AutoIncrement

---
+++ SpringData/HibernateCodeFirstLab/Joined AutoIncrement/src/main/java/entities/Plane.java
@@ -0,0 +1,31 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "planes")
+public class Plane extends Vehicle {
+    private static final String TYPE = "Plane";
+
+    private Integer passengerCapacity;
+
+    public Plane() {
+        super(TYPE);
+    }
+
+    public Plane(String model, String fuelType, Integer passengerCapacity) {
+        this();
+        this.model = model;
+        this.fuelType = fuelType;
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public Integer getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(Integer passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+}
