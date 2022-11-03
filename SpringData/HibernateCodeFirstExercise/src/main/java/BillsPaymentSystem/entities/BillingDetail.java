61bd9605e052302dc2bc216d5b0980737d2ade39
Georgi Petrov <gap4400@gmail.com>
2022-11-03 19:11:10 +0200

    BillsPaymentSystem edit

--- SpringData/HibernateCodeFirstExercise/src/main/java/BillsPaymentSystem/entities/BillingDetail.java
+++ SpringData/HibernateCodeFirstExercise/src/main/java/BillsPaymentSystem/entities/BillingDetail.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "billing_details")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class BillingDetail {
 
     @Id
